5d7b3a850c85f11556062d11aadea18bfa523499
dorabon <wirateds@gmail.com>
2015-12-03 21:50:14 +0700

    first commit

---
+++ app/src/main/java/itos/ilawyer/detail.java
@@ -0,0 +1,13 @@
+package itos.ilawyer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
